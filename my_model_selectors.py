2b3d7826f46c478694665c9376e0961961850d0d
alexgyl <gyl@u.nus.edu>
2017-08-22 17:08:20 +0800

    updated with working BIC function

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -75,9 +75,55 @@ class SelectorBIC(ModelSelector):
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        ## Implement model selection using CV
+        # Initial values
+        best_score = float("Inf")
+        best_num_states = 2
+        ## Iterate through a number of states to test which is the best representation
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            BIC_score = 0
+            ## Perform CV split
+            # Case if we have less data than samples (here we use KFold = 3 folds)
+            if len(self.sequences) < 3:
+                n_split = math.ceil(0.75 * len(self.sequences))
+            else:
+                n_split = 3
+            ## Create the split method & fix the seeding
+            split_method = KFold(n_splits = n_split, random_state = self.random_state)
+            ## KFold CV
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                try:
+                    # Split the sequences accordingly using helper function to split the data correct
+                    sequence_split_train = combine_sequences(cv_train_idx, self.sequences)
+                    sequence_split_cv = combine_sequences(cv_test_idx, self.sequences)
+                    # HMM Model building - num_states is our parameter that is found using CV
+                    hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                random_state=self.random_state, verbose=False).fit(sequence_split_train[0], sequence_split_train[1])
+                    if self.verbose:
+                        print("model created for {} with {} states".format(self.this_word, num_states))
+                    # Log-likelihood score
+                    logL = hmm_model.score(sequence_split_cv[0], sequence_split_cv[1])
+                    # BIC score
+                    BIC_score += (-2 * logL) + (num_states * math.log(sequence_split_train[1]))
+                except:
+                    if self.verbose:
+                        print("failure on {} with {} states".format(self.this_word, num_states))
+                        return None
+            
+            ## Average the score across all the folds - KFold is fixed to 3 at the moment, except if there's too little data
+            BIC_score /= n_split
+            ## Tracking best score and best number of states parameter - the lower the BIC the better
+            if BIC_score < best_score:
+                best_score = BIC_score
+                best_num_states = num_states
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        ## Build the best hmm model using all data once parameter has been finalized
+        best_hmm_model = GaussianHMM(n_components=best_num_states, covariance_type="diag", n_iter=1000,
+                        random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+        if self.verbose:
+            print("Best model created for {} with {} states".format(self.this_word, best_num_states))
+
+        return best_hmm_model
 
 
 class SelectorDIC(ModelSelector):
@@ -93,8 +139,55 @@ class SelectorDIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        ## Implement model selection using CV
+        # Initial values
+        best_score = float("Inf")
+        best_num_states = 2
+        ## Iterate through a number of states to test which is the best representation
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
+            DIC_score = 0
+            ## Perform CV split
+            # Case if we have less data than samples (here we use KFold = 3 folds)
+            if len(self.sequences) < 3:
+                n_split = math.ceil(0.75 * len(self.sequences))
+            else:
+                n_split = 3
+            ## Create the split method & fix the seeding
+            split_method = KFold(n_splits = n_split, random_state = self.random_state)
+            ## KFold CV
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                try:
+                    # Split the sequences accordingly using helper function to split the data correct
+                    sequence_split_train = combine_sequences(cv_train_idx, self.sequences)
+                    sequence_split_cv = combine_sequences(cv_test_idx, self.sequences)
+                    # HMM Model building - num_states is our parameter that is found using CV
+                    hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                random_state=self.random_state, verbose=False).fit(sequence_split_train[0], sequence_split_train[1])
+                    if self.verbose:
+                        print("model created for {} with {} states".format(self.this_word, num_states))
+                    # Log-likelihood score
+                    logL = hmm_model.score(sequence_split_cv[0], sequence_split_cv[1])
+                    # DIC score
+                    DIC_score += (-2 * logL) + (num_states * math.log(sequence_split_train[1]))
+                except:
+                    if self.verbose:
+                        print("failure on {} with {} states".format(self.this_word, num_states))
+                        return None
+            
+            ## Average the score across all the folds - KFold is fixed to 3 at the moment, except if there's too little data
+            DIC_score /= n_split
+            ## Tracking best score and best number of states parameter - the lower the BIC the better
+            if DIC_score < best_score:
+                best_score = DIC_score
+                best_num_states = num_states
 
+        ## Build the best hmm model using all data once parameter has been finalized
+        best_hmm_model = GaussianHMM(n_components=best_num_states, covariance_type="diag", n_iter=1000,
+                        random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+        if self.verbose:
+            print("Best model created for {} with {} states".format(self.this_word, best_num_states))
+
+        return best_hmm_model
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
@@ -104,25 +197,28 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        best_score = 0
-        best_num_states = 1
+        ## Implement model selection using CV
+        # Initial values
+        best_score = float("-Inf")
+        best_num_states = 2
         ## Iterate through a number of states to test which is the best representation
-        for num_states in range(1,5):
+        for num_states in range(self.min_n_components, self.max_n_components + 1):
             score = 0
             ## Perform CV split
+            # Case if we have less data than samples (here we use KFold = 3 folds)
             if len(self.sequences) < 3:
                 n_split = math.ceil(0.75 * len(self.sequences))
             else:
                 n_split = 3
             ## Create the split method & fix the seeding
-            # print("Final n_split:{}\n".format(n_split))
-            # print("This word is:{}\n".format(self.this_word))
             split_method = KFold(n_splits = n_split, random_state = self.random_state)
+            ## KFold CV
             for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
                 try:
+                    # Split the sequences accordingly using helper function to split the data correct
                     sequence_split_train = combine_sequences(cv_train_idx, self.sequences)
                     sequence_split_cv = combine_sequences(cv_test_idx, self.sequences)
+                    # HMM Model building - num_states is our parameter that is found using CV
                     hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                 random_state=self.random_state, verbose=False).fit(sequence_split_train[0], sequence_split_train[1])
                     if self.verbose:
@@ -133,17 +229,17 @@ class SelectorCV(ModelSelector):
                         print("failure on {} with {} states".format(self.this_word, num_states))
                         return None
             
-            ## Average the score across all the folds - KFold is fixed to 3 at the moment
-            score /= 3
+            ## Average the score across all the folds - KFold is fixed to 3 at the moment, except if there's too little data
+            score /= n_split
             ## Tracking best score and best number of states parameter
             if score > best_score:
                 best_score = score
                 best_num_states = num_states
-        ## Build the best hmm model using all data
+
+        ## Build the best hmm model using all data once parameter has been finalized
         best_hmm_model = GaussianHMM(n_components=best_num_states, covariance_type="diag", n_iter=1000,
-                        random_state=self.random_state, verbose=True).fit(self.X, self.lengths)
+                        random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
         if self.verbose:
             print("Best model created for {} with {} states".format(self.this_word, best_num_states))
 
         return best_hmm_model
-        # return None
